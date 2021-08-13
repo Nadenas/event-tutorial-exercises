baa1f9f85c6dc61346afb0f2f1a8b8dd92e584f4
Almudena De La Rosa <almudenaxico@gmail.com>
2021-08-13 07:56:41 +0000

    first commit

--- exercises/05-the-load-event/index.js
+++ exercises/05-the-load-event/index.js
@@ -1,1 +1,8 @@
 // your function goes here
+/*document.addEventListener("load", function loadListener (event) {
+    alert("loading finished...");
+});*/
+function loadListener() {
+    alert("loading finished..." );
+}
+window.onload = loadListener;
